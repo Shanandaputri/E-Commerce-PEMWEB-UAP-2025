6ff786f97d409b55047b571b9e47787cac3f7488
Shanandaputri <shanandaputri1504@gmail.com>
2025-12-08 14:49:41 +0700

    Perbaiki database

--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -11,18 +11,17 @@ class ProductCategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            'Gaun & Dress',
-            'Blouse & Kemeja',
-            'Rok & Celana',
-            'Outerwear (Blazer & Coat)',
-            'Set Hijab & Aksesoris',
+            'T-Shirt',
+            'Hoodie / Jacket',
+            'Kemeja',
+            'Celana',
+            'Aksesoris Fashion',
         ];
 
         foreach ($categories as $name) {
             DB::table('product_categories')->insert([
                 'name'        => $name,
                 'slug'        => Str::slug($name),
-                'description' => 'Kategori ' . $name . ' untuk koleksi pakaian elegan.',
             ]);
         }
     }
--- database/seeders/ProductImageSeeder.php
+++ database/seeders/ProductImageSeeder.php
@@ -9,13 +9,29 @@ class ProductImageSeeder extends Seeder
 {
     public function run(): void
     {
-        // 1 gambar per produk, semua dijadikan thumbnail
-        for ($i = 1; $i <= 10; $i++) {
-            ProductImage::create([
-                'product_id'  => $i,
-                'image'       => "images/products/product-{$i}.jpg",
-                'is_thumbnail'=> true,
-            ]);
+        $jumlahKategori = 5;
+        $produkPerKategori = 3;
+        $fotoPerProduk = 3;
+
+        $totalProduk = $jumlahKategori * $produkPerKategori;
+
+        for ($product = 1; $product <= $totalProduk; $product++) {
+
+            // Hitung kategori saat ini
+            $kategori = ceil($product / $produkPerKategori);
+
+            // Produk ke berapa dalam kategori
+            $produkKe = $product % $produkPerKategori;
+            if ($produkKe == 0) $produkKe = $produkPerKategori;
+
+            for ($foto = 1; $foto <= $fotoPerProduk; $foto++) {
+
+                ProductImage::create([
+                    'product_id'   => $product,
+                    'image'        => "images/products/product-{$kategori}-{$produkKe}-{$foto}.jpg",
+                    'is_thumbnail' => $foto === 1,  // Foto 1 jadi thumbnail
+                ]);
+            }
         }
     }
 }
